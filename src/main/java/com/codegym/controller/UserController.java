9a5179e165e359723639995d05f7ef177c3b04a8
Ho Duong <hxhduong4394@gmail.com>
2019-10-07 11:04:05 +0700

    Update ApplicationConfig & ApplicationInitializer

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -1,21 +1,5 @@
 package com.codegym.controller;
 
-import com.codegym.ApplicationConfig;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+public class UserController {
 
-public class UserController extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{ApplicationConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
 }
